37efdb42d3bbc74b6a04b726fe6653ed8fa5536d
Ethan Samuel <samuelethanc@gmail.com>
2024-05-24 11:04:06 -0500

    Progress on AddGame

--- src/Components/AddGame.js
+++ src/Components/AddGame.js
@@ -1,20 +1,75 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 
 function AddGame() {
     const gameURL=`https://664a82eaa300e8795d4227ab.mockapi.io/game`
+    const [title, setTitle] = useState('');
+    const [maxPlayers, setMaxPlayers] = useState('');
+    const [gameInfo, setGameInfo] = useState('');
 
-    const handleAdd = async () => {
+
+    useEffect(() =>{
+        axios.get(gameURL)
+        .then(res => {
+            console.log(res.data)
+        })
+        .catch(error => {
+            console.error('Error Adding connection at useEffect', error);
+        })
+    }, []);
+
+    const handleSubmit = async () => {
+        // event.preventDefault();
+
+        const newGame = {
+            title: title,
+            maxPlayers: maxPlayers,
+            gameInfo: gameInfo,
+        }
         try {
-            await axios.push(${gameURL});
+            await axios.post(gameURL, newGame);
+            console.log(`You have successfully added ${newGame.title}`)
         } catch (error) {
             console.error('Error Adding Connection', error);
         }
     }
     // you left off here, you're looking to make a form to handle the data that would add to the API
     return (
-    <div>
-        
-    </div>
+<div>
+            <h2>Add New User</h2>
+            <form onSubmit ={handleSubmit}>
+                <div>
+                    <label>Game Title:</label>
+                    <input
+                        type="text"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Maximum Players:</label>
+                    <input
+                    type="number"
+                    value={maxPlayers}
+                    onChange={(e) => setMaxPlayers(e.target.value)}
+                    required
+                    />
+                </div>
+                <div>
+                    <label>Game Description</label>
+                    <input
+                    type="text"
+                    value={gameInfo}
+                    onChange={(e) => setGameInfo(e.target.value)}
+                    required
+                    />
+                </div>
+                <button onclick={handleSubmit}>
+                    Add a New Connection
+                </button>
+            </form>
+        </div>
   )
 }
 
--- src/Components/Pages/Games.js
+++ src/Components/Pages/Games.js
@@ -1,11 +1,14 @@
 import React, {useEffect, useState} from 'react'
 import { Navbar, Nav, Form, FormControl, Button, Dropdown, DropdownButton, Card } from 'react-bootstrap';
 import GameDropdown from '../GameDropdown';
+import AddGame from '../AddGame';
 
 const GameList = () => {
     return (
-        <GameDropdown/>
-
+        <>
+            <GameDropdown/>
+            <AddGame/>
+        </>
     )
 
 
